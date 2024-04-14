27fbbb497ecc8e98c7687b90f25eba06dd137e32
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-04-14 09:19:30 -0500

    Added setup for fetching from api

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log('Hello world');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const wordSearchForm = document.getElementById('word-search-form');
+const copyBtn = document.getElementById('copy-btn');
+const wordAppBody = document.querySelector('.word-app-body');
+const wordListContainer = document.getElementById('word-list');
+const loadingSpinner = document.getElementById('spinner');
+let wordsNotFound = true;
+const getInputWord = () => {
+    wordSearchForm === null || wordSearchForm === void 0 ? void 0 : wordSearchForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        let searchWord = wordSearchForm.search_word.value;
+        fetchSynWords(searchWord);
+    });
+};
+getInputWord();
+const fetchSynWords = (searchWord) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(searchWord);
+    let url = `https://api.datamuse.com/words?rel_syn=${searchWord}`;
+    try {
+        loadingSpinner.style.display = "flex";
+        let res = yield fetch(url);
+        let;
+    }
+    finally {
+    }
+});
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello world')
+const wordSearchForm = document.getElementById('word-search-form');
+const copyBtn = document.getElementById('copy-btn');
+const wordAppBody = document.querySelector('.word-app-body');
+const wordListContainer = document.getElementById('word-list');
+const loadingSpinner = document.getElementById('spinner');
+
+let wordsNotFound: boolean = true;
+
+
+const getInputWord = () => {
+    wordSearchForm?.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        let searchWord = wordSearchForm.search_word.value;
+        fetchSynWords(searchWord);
+    })
+}
+
+getInputWord()
+
+const fetchSynWords = async(searchWord: string) => {
+    console.log(searchWord)
+    let url = `https://api.datamuse.com/words?rel_syn=${searchWord}`
+    try {
+        loadingSpinner!.style.display = "flex";
+        let res = await fetch(url);
+        let 
+    }
+}
